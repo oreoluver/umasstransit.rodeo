6ce9eee7d1427933955d73a4cb61f84aebb61906
Arta <artarazavi@gmail.com>
2016-06-02 14:34:56 -0400

    Experimenting

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   as :user do
     get 'users/edit', to: 'devise/registrations#edit', as: :edit_user_registration
-    put 'users', to: 'devise/registrations#update', as: :user_registration           
+    put 'users', to: 'devise/registrations#update', as: :user_registration 
+    #get 'users/signup', to: 'devise/registrations#create', as: :create_user_registration 
   end
 
   root 'participants#welcome'
